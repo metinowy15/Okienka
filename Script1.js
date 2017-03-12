39903759b771f4f0d70b5ef17affcf946ae71940
Mrowca Jakub <jakub.mrowca@microsoft.wsei.edu.pl>
2017-03-12 17:16:48 +0100

    Dodaje

---
+++ Script1.js
@@ -0,0 +1,16 @@
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    console.log("sdsds");
+
+    var przycisk = document.getElementById("przycisk");
+    przycisk.addEventListener("click", uzupelnij);
+
+}
+function uzupelnij() {
+
+   var stronaDwa=document.getElementById("ja");
+   stronaDwa.innerHTML = "<h2>Lubie czytac</h2><h2>Lubie Grac</h2><h2>Lubie Jesc</h2><h2>Lubie Programowac</h2>"
+    stronaDwa.da
+   
+}
